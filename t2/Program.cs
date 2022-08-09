28d9057609855951a174062be0a0026c262701b7
Rishat <risatnurullin53@gmail.com>
2022-08-09 21:14:39 +0300

    ytuu

---
+++ t2/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = new int[8];
+    Console.WriteLine($"Введите числа для массива из {array.Length}-ми элементов");
+
+    for (int i = 0; i < array.Length;  i++)
+    {
+        Console.Write($"{i + 1}й элемент массива ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+Console.WriteLine($"Введен следующий массив [{string.Join(", ", array)}]");
