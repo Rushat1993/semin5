9e1f84a8542e0c7b3038a147f14be4fceeec829a
Rishat <risatnurullin53@gmail.com>
2022-08-09 21:15:17 +0300

    ytuuo

---
+++ t3/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange);
+    }
+    return arr;
+}
+
+int[] array = FillArrayWithRandomNumbers(8, 100, 1000);
+
+int cnt = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            cnt = cnt + 1;
+        }
+    }
+
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine("Количество чётных чисел = " + cnt);
