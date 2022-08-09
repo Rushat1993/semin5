0e05f75feca49b3b8fa81c475b900373d8a8416e
Rishat <risatnurullin53@gmail.com>
2022-08-09 21:15:46 +0300

    ytuuol

---
+++ t4/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange);
+    }
+    return arr;
+}
+
+int[] array = FillArrayWithRandomNumbers(123, 0, 170);
+
+int cnt = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            cnt = cnt + 1;
+        }
+    }
+
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine("количество элементов = " + cnt);
