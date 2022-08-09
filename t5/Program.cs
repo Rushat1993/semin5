32f688fb7eae877f71c04c0d90afeda46803d6d0
Rishat <risatnurullin53@gmail.com>
2022-08-09 21:16:18 +0300

    ytuuolk

---
+++ t5/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange);
+    }
+    return arr;
+}
+
+int[] array = FillArrayWithRandomNumbers(7, -100, 100);
+
+int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum = sum + array[i];
+    }
+
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine("Сумма элементов = " + sum);
